965ccebdf9695e14f75dc64604647a2920e3978e
Ubaid-ch <80938435+Ubaid-ch@users.noreply.github.com>
2022-07-09 12:04:44 +0500

    server

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const http = require("http")
+const fs = require("fs")
 
 const server= http.createServer((req, res) => {
     // console.log("req made")
     console.log(req.url, req.method)
+
+    // setting header type
     res.setHeader("Content-Type", "text/html")
-    res.write("<h1>AoA Ubaid</h1>")
-    res.write("<p>kesy ho</p>")
-    res.end()
+//     res.write("<h1>AoA Ubaid</h1>")
+//     res.write("<p>kesy ho</p>")
+//     res.end()
+
+// sending html file
+ fs.readFile("./html/first.html", (err, data) => {
+    if(err){
+        console.log("err")
+        res.end()
+    }
+    // res.write(data)
+    // res.end()
+
+    res.end(data)
+ })
+
 })
 
 server.listen(3000, "localhost", ()=>{
