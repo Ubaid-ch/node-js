537712f52eb7a6d28a8cb4cbf48a73f316c8ab64
Ubaid-ch <80938435+Ubaid-ch@users.noreply.github.com>
2022-07-09 11:33:09 +0500

    req and res

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http")
 
 const server= http.createServer((req, res) => {
-    console.log("req made")
+    // console.log("req made")
+    console.log(req.url, req.method)
+    res.setHeader("Content-Type", "text/html")
+    res.write("<h1>AoA Ubaid</h1>")
+    res.write("<p>kesy ho</p>")
+    res.end()
 })
 
 server.listen(3000, "localhost", ()=>{
